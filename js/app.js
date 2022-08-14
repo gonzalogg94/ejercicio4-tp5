0618b348995924bbaac182236b7828800076c48d
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-08-14 11:51:12 -0300

    termine cambio de colores de reloj

--- js/app.js
+++ js/app.js
@@ -1,18 +1,9 @@
 function obtenerHora(){
     let fechaActual = new Date();
-    // console.log(fechaActual)
-    // console.log(fechaActual.getDate()); // 1-31
-    // console.log(fechaActual.getDay());
-    // console.log(fechaActual.getMonth());
-    // console.log(fechaActual.getFullYear());
     
     let diaSemanas = ['Domingo', 'Lunes', 'Martes', 'Miercoles','Jueves','Viernes', 'Sabado'];
     let meses = ['Enero', 'Febrero', 'Marzo', 'Abril','Mayo','Junio', 'Julio','Agosto','Septiembre', 'Octubre','Noviembre','Diciembre'];
     
-    // console.log(diaSemanas[fechaActual.getDay()]);
-    // console.log(meses[fechaActual.getMonth()]);
-    
-    //  <p>Jueves 11 de agosto de 2022</p>
     let parrafoFecha = document.querySelector('#fecha');
     parrafoFecha.innerHTML = `${diaSemanas[fechaActual.getDay()]} ${fechaActual.getDate()} de ${meses[fechaActual.getMonth()]} del ${fechaActual.getFullYear()}`;
     let parrafoHora = document.querySelector('#hora');
@@ -25,3 +16,17 @@ function obtenerHora(){
 }
 
 setInterval(obtenerHora,1000);
+
+
+function CambiarColorAzul(){
+    let titulo = document.querySelector("#cambiarColor");
+    titulo.className = "titulo fondoAzul";
+}
+function CambiarColorRosa(){
+    let titulo = document.querySelector("#cambiarColor");
+    titulo.className = "titulo fondoRosa";
+}
+function CambiarColorVerde(){
+    let titulo = document.querySelector("#cambiarColor");
+    titulo.className = "titulo fondoVerde";
+}
